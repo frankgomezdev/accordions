d0835118bc5359aa6e64e14398375d432783d1cb
frankgomezdev <franklingomezdev@gmail.com>
2026-03-19 13:09:49 -0400

    feat: created accordion item and mapping in Accordion

--- src/Accordion.tsx
+++ src/Accordion.tsx
@@ -1,25 +1,26 @@
+import { useState } from "react";
+import AccordionItem from "./AccordionItem";
 
-function Accordion() {
+function Accordion({ items }) {
+const {activeIndex, setActiveIndex} = useState(null)
 
-const items = [
-          {
-            title: "What is Github and how does it work?",
-            content:
-              "GitHub is the home for all developers—a platform where you can share code, contribute to open source projects, or even automate your workflow with tools like GitHub Actions and Packages. If you’re just getting started with GitHub, you may know us best as a place for version control and collaboration.",
-          },
-          {
-            title: "How do I see GitHub's availability?",
-            content: "Check our real-time status report",
-          },
-          {
-            title: "Why is GitHub so popular?",
-            content:
-              "GitHub is built by developers for developers, and we’re proud to be home to the world’s largest open source community. With 50 million developers and millions more open source projects, GitHub has become the go-to place to collaborate and build software together.",
-          },
-];    
+const handleClick = (index) => {
+    setActiveIndex(prevIndex =>(prevIndex === index ? null : index))
+}
+ 
 
   return (
-    <div>Accordion</div>
+    <div>
+        {items.map((item, index) => (
+            <AccordionItem
+                key={index} 
+                title={item.title} 
+                content={item.content}
+                isOpen={activeIndex === index} 
+                onClick={() => handleClick(index)}
+            />
+        ))}
+    </div>
   )
 }
 
